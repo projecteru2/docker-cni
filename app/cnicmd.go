b8a71b462c9674e2c2230ca1843908331f618812
zc <greyschwinger@gmail.com>
2021-12-16 14:17:22 +0800

    allow to adjust cni config dynamically (#7)
    * allow adjust cni config dynamically
    
    * inject hooks with cni_args env

--- app/cnicmd.go
+++ app/cnicmd.go
@@ -12,58 +12,73 @@ import (
 	"github.com/pkg/errors"
 	"github.com/projecteru2/docker-cni/cni"
 	"github.com/projecteru2/docker-cni/config"
+	"github.com/projecteru2/docker-cni/handler"
 	"github.com/urfave/cli/v2"
 )
 
-func runCNI(c *cli.Context) (err error) {
-	stateBuf, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	var state specs.State
-	if err = json.Unmarshal(stateBuf, &state); err != nil {
-		return errors.WithStack(err)
-	}
+func runCNI(handler handler.Handler) func(*cli.Context) error {
+	return func(c *cli.Context) (err error) {
+		stateBuf, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		var state specs.State
+		if err = json.Unmarshal(stateBuf, &state); err != nil {
+			return errors.WithStack(err)
+		}
 
-	conf, err := config.LoadConfig(c.String("config"))
-	if err != nil {
-		return
-	}
+		conf, err := config.LoadConfig(c.String("config"))
+		if err != nil {
+			return
+		}
 
-	cniFilename, cniConfigFilename, err := cni.FindCNI(conf.CNIConfDir, conf.CNIBinDir)
-	if err != nil {
-		return
-	}
+		cniFilename, cniConfigFilename, err := cni.FindCNI(conf.CNIConfDir, conf.CNIBinDir)
+		if err != nil {
+			return
+		}
 
-	env := []string{
-		"CNI_IFNAME=" + conf.CNIIfname,
-		"CNI_PATH=" + conf.CNIBinDir,
-		"CNI_ARGS=" + os.Getenv("CNI_ARGS"),
-		"CNI_COMMAND=" + strings.ToUpper(c.String("command")),
-		"CNI_CONTAINERID=" + state.ID,
-	}
+		env := []string{
+			"CNI_IFNAME=" + conf.CNIIfname,
+			"CNI_PATH=" + conf.CNIBinDir,
+			"CNI_ARGS=" + os.Getenv("CNI_ARGS"),
+			"CNI_COMMAND=" + strings.ToUpper(c.String("command")),
+			"CNI_CONTAINERID=" + state.ID,
+		}
 
-	if state.Pid != 0 {
-		env = append(env, "CNI_NETNS="+fmt.Sprintf("/proc/%d/ns/net", state.Pid))
-	}
+		if state.Pid != 0 {
+			env = append(env, "CNI_NETNS="+fmt.Sprintf("/proc/%d/ns/net", state.Pid))
+		}
 
-	file, err := os.Open(cniConfigFilename)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	if err := syscall.Dup2(int(file.Fd()), 0); err != nil {
-		return errors.WithStack(err)
-	}
+		cniConfig, err := os.ReadFile(cniConfigFilename)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		if cniConfig, err = handler.HandleCNIConfig(cniConfig); err != nil {
+			return errors.WithStack(err)
+		}
+		r, w, err := os.Pipe()
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		if _, err = w.Write(cniConfig); err != nil {
+			return errors.WithStack(err)
+		}
+		defer w.Close()
+		if err := syscall.Dup2(int(r.Fd()), 0); err != nil {
+			return errors.WithStack(err)
+		}
 
-	if file, err = os.OpenFile(conf.CNILog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err != nil {
-		return errors.WithStack(err)
-	}
-	if err := syscall.Dup2(int(file.Fd()), 1); err != nil {
-		return errors.WithStack(err)
-	}
-	if err := syscall.Dup2(int(file.Fd()), 2); err != nil {
-		return errors.WithStack(err)
-	}
+		file, err := os.OpenFile(conf.CNILog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		if err := syscall.Dup2(int(file.Fd()), 1); err != nil {
+			return errors.WithStack(err)
+		}
+		if err := syscall.Dup2(int(file.Fd()), 2); err != nil {
+			return errors.WithStack(err)
+		}
 
-	return errors.WithStack(syscall.Exec(cniFilename, []string{cniFilename}, env))
+		return errors.WithStack(syscall.Exec(cniFilename, []string{cniFilename}, env))
+	}
 }
